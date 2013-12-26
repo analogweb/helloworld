145059111fda83d84d540bd3f9572e7aa1abc929
snowgooseyk <snowgoose.yk@gmail.com>
2013-12-27 00:51:08 +0900

    Refine

--- src/main/java/org/analogweb/helloworld/HelloWorld.java
+++ src/main/java/org/analogweb/helloworld/HelloWorld.java
@@ -1,11 +1,16 @@
 package org.analogweb.helloworld;
 
+import static org.analogweb.core.response.BasicResponses.text;
+import static org.analogweb.core.response.BasicResponses.xml;
+
 import java.net.URI;
 
-import static org.analogweb.core.response.BasicResponses.*;
 import org.analogweb.Renderable;
 import org.analogweb.annotation.Bean;
+import org.analogweb.annotation.Get;
 import org.analogweb.annotation.PathVariable;
+import org.analogweb.annotation.Post;
+import org.analogweb.annotation.Put;
 import org.analogweb.annotation.RequestFormats;
 import org.analogweb.annotation.Route;
 import org.analogweb.annotation.XmlType;
@@ -28,28 +33,33 @@ public class HelloWorld {
     }
 
     @Route
+    @Get
     public String helloworld() {
         return "Hello World";
     }
 
     @Route("hello/{name}/world")
+    @Get
     public Text helloworld(@PathVariable("name") String name) {
         return text(String.format("Hello %s World", name));
     }
 
     @Route
+    @Get
     public Text helloUserAgent(@UserAgent String userAgent) {
         return text(String.format("Hello World %s", userAgent));
     }
 
     @Route
+    @Get
     public Xml helloXml() {
         FooBean foo = new FooBean();
         return xml(foo);
     }
 
     @Route
-    @RequestFormats(MediaTypes.APPLICATION_XML)
+    @RequestFormats
+    @Put
     public Renderable helloXmlValue(@XmlType FooBean foo) {
         if (foo == null) {
             return HttpStatus.BAD_REQUEST.byReasonOf("XML entity required.");
@@ -58,6 +68,9 @@ public class HelloWorld {
     }
 
     @Route
+    @RequestFormats(MediaTypes.APPLICATION_FORM_URLENCODED)
+    @Put
+    @Post
     public Xml helloBean(@Bean FooBean foo) {
         return xml(foo);
     }
